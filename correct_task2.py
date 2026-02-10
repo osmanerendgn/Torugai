6368a599806866c2eb4f2a713e435c7e8c962e69
osmanerendgn <139166538+osmanerendgn@users.noreply.github.com>
2026-02-10 22:19:53 +0300

    Update correct_task2.py
    Fixed the possible errors

--- correct_task2.py
+++ correct_task2.py
@@ -1,2 +1,12 @@
-# Write your corrected implementation for Task 2 here.
-# Do not modify `task2.py`.
+import re
+import pandas as pd
+
+def count_valid_emails(emails):
+    if not isinstance(emails, list):
+        return 0
+
+    s = pd.Series(emails)
+    clean_s = s.dropna().astype(str).str.strip()
+    email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    valid_emails = clean_s.str.match(email_regex)
+    return valid_emails.sum()
